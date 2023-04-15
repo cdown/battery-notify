7f170dddcef41277d9679a3de33536327c4a8da6
Chris Down <chris@chrisdown.name>
2023-04-16 00:46:32 +0100

    feat: Add support for reading _full and _now

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use std::cell::Cell;
 use std::ffi::OsStr;
 use std::fs;
-use std::path::PathBuf;
+use std::path::Path;
 
 thread_local! {
     static LAST_BATTERY_STATE: Cell<BatteryState> = Cell::new(BatteryState::Unknown);
@@ -20,6 +20,10 @@ enum BatteryState {
 #[derive(Debug)]
 struct Battery {
     state: BatteryState,
+
+    // Unitless, may either come from charge_* or energy_* since we just use it for percentage
+    full: u64,
+    now: u64,
 }
 
 fn set_last_state(state: BatteryState) {
@@ -30,12 +34,22 @@ fn get_last_state() -> BatteryState {
     LAST_BATTERY_STATE.with(Cell::get)
 }
 
-fn read_battery_file(dir: PathBuf, file: &str) -> Result<String> {
-    let mut content = fs::read_to_string(dir.join(file))?;
+fn read_battery_file(dir: &Path, file: impl AsRef<str>) -> Result<String> {
+    let mut content = fs::read_to_string(dir.join(file.as_ref()))?;
     if let Some(idx) = content.find('\n') {
         content.truncate(idx);
     }
     Ok(content)
+}
+
+/// Some drivers expose coloumb counter (charge), some drivers expose µWh (energy), some drivers
+/// expose both. We only care about the percentage.
+fn read_battery_file_energy_or_charge(dir: &Path, partial_file: &str) -> Result<String> {
+    let energy = read_battery_file(dir, "energy".to_owned() + partial_file);
+    if energy.is_ok() {
+        return energy;
+    }
+    read_battery_file(dir, "charge".to_owned() + partial_file)
 }
 
 fn name_to_battery_state(name: &str) -> BatteryState {
@@ -48,9 +62,12 @@ fn name_to_battery_state(name: &str) -> BatteryState {
     }
 }
 
-fn read_battery_dir(dir: PathBuf) -> Result<Battery> {
+fn read_battery_dir(dir: impl AsRef<Path>) -> Result<Battery> {
+    let dir = dir.as_ref();
     Ok(Battery {
         state: name_to_battery_state(&read_battery_file(dir, "status")?),
+        full: read_battery_file_energy_or_charge(dir, "_full")?.parse()?,
+        now: read_battery_file_energy_or_charge(dir, "_now")?.parse()?,
     })
 }
 
