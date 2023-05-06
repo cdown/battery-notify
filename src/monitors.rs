e7fa82e2d26e0213cee5fca2a954d0a522135db9
Chris Down <chris@chrisdown.name>
2023-05-06 03:02:16 +0100

    monitors: Bail early if we cannot connect to X

--- src/monitors.rs
+++ src/monitors.rs
@@ -2,32 +2,20 @@ use anyhow::Result;
 
 #[cfg(feature = "mons")]
 pub fn get_nr_connected() -> Result<usize> {
-    use anyhow::bail;
-    use log::error;
     use once_cell::sync::Lazy;
     use x11rb::{connection::Connection, protocol::randr, rust_connection::RustConnection};
 
-    static CONN_AND_ROOT: Lazy<Option<(RustConnection, u32)>> = Lazy::new(|| {
-        let conn_and_root = x11rb::connect(None).map(|(c, screen)| {
-            let root = c.setup().roots[screen].root;
-            (c, root)
-        });
-        match conn_and_root {
-            Ok((conn, root)) => Some((conn, root)),
-            Err(err) => {
-                error!("Failed to connect to X, will not be able to retrieve monitor information: {err}");
-                None
-            }
-        }
+    static CONN_AND_ROOT: Lazy<(RustConnection, u32)> = Lazy::new(|| {
+        x11rb::connect(None)
+            .map(|(c, screen)| {
+                let root = c.setup().roots[screen].root;
+                (c, root)
+            })
+            .unwrap()
     });
 
-    let (conn, root) = match Lazy::force(&CONN_AND_ROOT) {
-        Some((conn, root)) => (conn, root),
-        None => bail!("No X connection"),
-    };
-
+    let (conn, root) = Lazy::force(&CONN_AND_ROOT);
     let resources = randr::get_screen_resources(conn, *root)?;
-
     let mut nr_connected = 0;
     for output in resources.reply()?.outputs {
         let output_info = randr::get_output_info(conn, output, 0)?.reply()?;
