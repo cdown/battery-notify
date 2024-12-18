c85f138f889d81aeffe4dd98c3c82a9bb45590e6
Chris Down <chris@chrisdown.name>
2024-12-18 03:07:03 +0000

    notification: Don't inhibit notifs with the same summary after close
    If SingleNotification::close() is called, a subsequent call to show()
    with the same summary will fail to display the notification, because the
    summary field is not reset, causing the comparison in `show()` to block
    re-creation.
    
    This can prevent (for example) low battery notifications from displaying
    again after being closed, as described in #79.
    
    The summary field is now an Option<String> and is cleared in close(),
    allowing notifications with the same summary to be displayed after the
    previous one is closed.
    
    Fixes #79.

--- src/notification.rs
+++ src/notification.rs
@@ -4,27 +4,29 @@ use notify_rust::{Notification, NotificationHandle, Urgency};
 #[derive(Default)]
 pub struct SingleNotification {
     hnd: Option<NotificationHandle>,
-    summary: String,
+    summary: Option<String>,
 }
 
 impl SingleNotification {
     pub fn show(&mut self, summary: String, urgency: Urgency) {
-        if self.summary != summary {
+        if self.summary.as_ref() != Some(&summary) {
             self.close();
-            self.summary = summary;
-            trace!("Creating notification for {}", self.summary);
+            trace!("Creating notification for {}", summary);
             self.hnd = Notification::default()
-                .summary(&self.summary)
+                .summary(&summary)
                 .urgency(urgency)
                 .show()
                 .map_err(|err| error!("error showing notification: {err}"))
                 .ok();
+            self.summary = Some(summary)
         }
     }
 
     pub fn close(&mut self) {
         if let Some(hnd) = self.hnd.take() {
-            trace!("Closing notification for {}", self.summary);
+            if let Some(summary) = self.summary.take() {
+                trace!("Closing notification for {}", summary);
+            }
             hnd.close();
         }
     }
